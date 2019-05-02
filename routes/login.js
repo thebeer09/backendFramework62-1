e4fa7bcb4cbd0efc58f8e8633fd0170a2ffc60fc
thebeer09 <thebeer09@gmail.com>
2019-05-02 10:44:36 +0700

    Add generate jwt.

--- routes/login.js
+++ routes/login.js
@@ -10,92 +10,6 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/c", (req, res) => {
-    var nameVar = req.body.name; // name คือตัวที่พิมลง postman
-    var passwordVar = req.body.password; // password พิมค่าลง postman
-    MongoClient.connect(
-        "mongodb://framework:framework62@ds147096.mlab.com:47096/frameworkdb", {
-            useNewUrlParser: true
-        },
-        function (err, db) {
-            let dbo = db.db("frameworkdb");
-            let userObj = {
-                username: nameVar, // username คือค่า attribute ใน db
-                password: passwordVar // password คือค่า attribute ใน db
-            };
-            dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
-                if (err) throw err;
-                res.send({
-                    status: "store success",
-                    name: nameVar //response
-                });
-            });
-            db.close();
-        }
-    );
-});
-
-router.post("/register", (req, res) => {
-    var rankVar = req.body.rank;
-    var first_nameVar = req.body.first_name;
-    var last_nameVar = req.body.last_name;
-    var id_milVar = req.body.id_mil;
-    var unit_nameVar = req.body.unit_name;
-    var nameVar = req.body.username; // name คือตัวที่พิมลง postman
-    var passwordVar = req.body.password; // password พิมค่าลง postman
-    MongoClient.connect(
-        "mongodb+srv://weerayut:22374736@cluster0-4wunc.gcp.mongodb.net/newDatabase62?retryWrites=true", {
-            useNewUrlParser: true
-        },
-        function (err, db) {
-            if (err) {
-                res.sendStatus(404);
-                return;
-            }
-            let dbo = db.db("newDatabase62");
-            let userObj = {
-                rank: rankVar,
-                first_name: first_nameVar,
-                last_name: last_nameVar,
-                id_mil: id_milVar,
-                unit_name: unit_nameVar,
-                username: nameVar, // username คือค่า attribute ใน db
-                password: passwordVar // password คือค่า attribute ใน db
-            };
-            dbo.collection("userLoginTable").findOne({
-                username: nameVar
-            }, function (err, result) {
-                if (err) {
-                    res.send({
-                        status: false
-                    });
-                }
-                console.log(result);
-
-                if (result) {
-                    res.send({
-                        status: false,
-                        message: "มี username นี้แล้ว"
-                    });
-                } else {
-                    dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
-                        if (err) {
-                            res.send({
-                                status: false
-                            });
-                        }
-                        res.send({
-                            status: "store success",
-                            name: nameVar //response
-                        });
-                    });
-                }
-            });
-            db.close();
-        }
-    );
-});
-
 router.post("/login", (req, res) => {
     var nameVar = req.body.username; // name คือตัวที่พิมลง postman
     var passwordVar = req.body.password; // password พิมค่าลง postman
@@ -180,11 +94,11 @@ router.post("/register", (req, res) => {
             }, function (err, result) {
                 if (err) {
                     res.send({
-                        status: false,
-                        message: err.message
+                        status: false
                     });
                 }
                 console.log(result);
+
                 if (result) {
                     res.send({
                         status: false,
@@ -194,12 +108,12 @@ router.post("/register", (req, res) => {
                     dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
                         if (err) {
                             res.send({
-                                status: false,
-                                message: err.message
+                                status: false
                             });
                         }
                         res.send({
-                            status: true
+                            status: "store success",
+                            name: nameVar //response
                         });
                     });
                 }
